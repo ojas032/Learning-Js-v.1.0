333dfa836b6535d75d064d032bd4a46a918e75ce
root <root@localhost.localdomain>
2020-09-11 23:15:37 +0530

    problem with score function

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
-var score,roundscore,activeplayer,dice;
+var score,roundscore,activeplayer;
 score=[0,0];
 roundscore=0;
 activeplayer=0;
 
-dice=Math.floor(Math.random()*6)+1;
-console.log(dice);
+//dice=Math.floor(Math.random()*6)+1;
+//console.log(dice);
 
-document.querySelector('score-0').textContent=dice;
+//document.querySelector('#current-'+activeplayer).textContent=dice;
+
+document.querySelector('.dice').style.display='none';
+
+document.querySelector('.btn-roll').addEventListener('click',function(){
+    var dice=Math.floor(Math.random()*6)+1;
+    var diceDOM=document.querySelector('.dice');
+    diceDOM.style.display='block';
+    diceDOM.src='dice-'+dice+'.png';
+    var score=document.querySelector('#current-'+activeplayer).textContent;
+    score+=dice;
+    document.querySelector('#current-'+activeplayer).textContent=score;
+
+});
+
 
 
 
