736c26e02ba7b54d9076856a40ed1bb7c9886a90
root <root@localhost.localdomain>
2020-09-11 23:48:45 +0530

    solved dice problem

--- app.js
+++ app.js
@@ -36,6 +36,8 @@ document.querySelector('.btn-roll').addEventListener('click',function(){
         document.querySelector('.player-0-panel').classList.toggle('active');
         document.querySelector('.player-1-panel').classList.toggle('active');
 
+        document.querySelector('.dice').style.display='none';
+
     }
 
 });
