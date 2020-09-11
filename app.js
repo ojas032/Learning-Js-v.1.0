52f1d9ca44007c4a9584427991caecd0a1ec8cab
root <root@localhost.localdomain>
2020-09-11 23:42:32 +0530

    problem with score solved and added new feature

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ score=[0,0];
 roundscore=0;
 activeplayer=0;
 
-//dice=Math.floor(Math.random()*6)+1;
-//console.log(dice);
+var init=function(str){
+    document.getElementById(str).textContent='0';
+}
 
-//document.querySelector('#current-'+activeplayer).textContent=dice;
+init('score-0');
+init('score-1');
+init('current-1');
+init('current-0');
+
 
 document.querySelector('.dice').style.display='none';
 
@@ -15,9 +20,23 @@ document.querySelector('.btn-roll').addEventListener('click',function(){
     var diceDOM=document.querySelector('.dice');
     diceDOM.style.display='block';
     diceDOM.src='dice-'+dice+'.png';
+    if(dice!==1){
     var score=document.querySelector('#current-'+activeplayer).textContent;
-    score+=dice;
-    document.querySelector('#current-'+activeplayer).textContent=score;
+    var p=parseInt(score);
+    p+=dice;
+    document.querySelector('#current-'+activeplayer).textContent=p;
+    }
+    else{
+        activeplayer+=1;
+        activeplayer%=2;
+        console.log(activeplayer);
+        init('current-1');
+        init('current-0');
+
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+    }
 
 });
 
