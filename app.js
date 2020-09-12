f0ab234eb8a90079921080edec08d47675fa7167
root <root@localhost.localdomain>
2020-09-12 19:27:21 +0530

    the is a bug a new variable needs to be added

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
 var score,roundscore,activeplayer;
-score=[0,0];
-roundscore=0;
-activeplayer=0;
 
 var init=function(str){
     document.getElementById(str).textContent='0';
 }
 
-init('score-0');
-init('score-1');
-init('current-1');
-init('current-0');
+var gameInit=function(){
+    score=[0,0];
+    roundscore=0;
+    activeplayer=0;
+    init('score-0');
+    init('score-1');
+    init('current-1');
+    init('current-0');
+
+    document.querySelector('#name-0').textContent='Player-1';
+    document.querySelector('#name-1').textContent='Player-2';
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.add('active');
+}
+
+gameInit();
+
+
+
+
 
 
 document.querySelector('.dice').style.display='none';
@@ -24,23 +40,51 @@ document.querySelector('.btn-roll').addEventListener('click',function(){
     var score=document.querySelector('#current-'+activeplayer).textContent;
     var p=parseInt(score);
     p+=dice;
+    roundscore=p;
     document.querySelector('#current-'+activeplayer).textContent=p;
     }
     else{
-        activeplayer+=1;
-        activeplayer%=2;
-        console.log(activeplayer);
-        init('current-1');
-        init('current-0');
-
-        document.querySelector('.player-0-panel').classList.toggle('active');
-        document.querySelector('.player-1-panel').classList.toggle('active');
-
-        document.querySelector('.dice').style.display='none';
+       nextPlayer();
 
     }
+});
+
+function nextPlayer(){
+    activeplayer+=1;
+    activeplayer%=2;
+    init('current-1');
+    init('current-0');
+    roundscore=0;
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+    document.querySelector('.dice').style.display='none';
+}
+
+
+
+
+document.querySelector('.btn-new').addEventListener('click',function(){
+    gameInit();
+});
+
+document.querySelector('.btn-hold').addEventListener('click',function(){
+
+    score[activeplayer]+=roundscore;
+
+    document.querySelector('#score-'+activeplayer).textContent=score[activeplayer];
+    if(score[activeplayer]>20){
+        document.querySelector('.dice').style.display='none';
+        document.querySelector('#name-'+activeplayer).textContent='Winner!';
+        document.querySelector('.player-'+activeplayer+'-panel').classList.add('winner');
+        document.querySelector('.player-'+activeplayer+'-panel').classList.remove('active');
+    }
+    else{
+        nextPlayer();
+    }
+
 
 });
+
 
 
 
